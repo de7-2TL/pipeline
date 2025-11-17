04ff7c1380b3665d4b823174141c175781bf81db
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2025-11-17 15:27:10 +0900

    feat: company_hook 추가 (#9)
    company_symbol을 담은 list 반환

---
+++ src/plugins/hooks/company_hook.py
@@ -0,0 +1,47 @@
+from abc import ABC
+from airflow.hooks.base import BaseHook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+
+class AbstractCompanyHook(BaseHook, ABC):
+    """
+    Source에서 회사 목록을 조회하는 Hook Interface
+    """
+    conn_id = None
+
+    def get_company_info(self) -> list[str]:
+        raise NotImplementedError()
+
+
+
+class SnowflakeCompanyHook(AbstractCompanyHook):
+    """
+    Snowflake의 COMPANY_INFO 테이블에서
+    회사의 company_symbol 리스트를 반환하는 Hook
+    """
+
+    def __init__(self, conn_id: str = "snowflake_conn") -> None:
+        super().__init__("company.task.hooks")
+        self.conn_id = conn_id
+
+    def get_company_info(self) -> list[str]:
+        """
+        company symbol list 조회
+        
+        Returns:
+            list[str]: 회사의 company_symbol 리스트
+        """
+
+
+        query = """
+            SELECT company_symbol
+            FROM dev.raw_data.COMPANY_INFO;
+        """
+
+        hook = SnowflakeHook(snowflake_conn_id=self.conn_id)
+        df = hook.get_pandas_df(query)
+
+        # company_symbol column → list[str]
+        return df["COMPANY_SYMBOL"].dropna().tolist()
+
+
