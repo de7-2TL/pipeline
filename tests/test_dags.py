b7097c180178b2863533a1cddfc6e27a5dc385a4
Seonggil Jeong <63443366+seon99il@users.noreply.github.com>
2025-11-18 17:46:12 +0900

    feat: init dbt project with Astronomer Cosmos (#26)
    * feat: init dbt project with Astronomer Cosmos

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,19 +1,31 @@
 import glob
 import os
+from unittest import mock
+
 import pytest
 from airflow.hooks.base import BaseHook
-from airflow.models import DagBag, Connection
+from airflow.models import Connection, DagBag
+from cosmos import ProjectConfig
+
+patcher = mock.patch("cosmos.ProjectConfig.validate_project", return_value=None)
+patcher.start()
+
+import dbt_dags  # noqa: E402
 
 DAG_PATH = os.path.join(os.path.dirname(__file__), "../src/dags")
 DAG_FILES = glob.glob(DAG_PATH, recursive=True)
 
 
 @pytest.fixture
-def dag_bag():
-    return DagBag(dag_folder=DAG_PATH, include_examples=False)
+def dag_bag(mocker):
+    mocker.patch.object(
+        dbt_dags,
+        "get_project_config",
+        side_effect=lambda project_name: ProjectConfig(
+            f"{DAG_PATH}/dbt/{project_name}"
+        ),
+    )
 
-
-def test_dag_loaded(dag_bag, mocker):
     mocker.patch.object(
         BaseHook,
         "get_conn",
@@ -24,4 +36,10 @@ def test_dag_loaded(dag_bag, mocker):
         ),
     )
 
+    yield DagBag(dag_folder=DAG_PATH, include_examples=False)
+
+    patcher.stop()
+
+
+def test_dag_loaded(dag_bag):
     assert not dag_bag.import_errors, f"DAG Import Errors: {dag_bag.import_errors}"
