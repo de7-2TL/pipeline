08303f44207f6da35745e91374695684c1600da1
minguk-cucu <minkuk0118@gmail.com>
2025-11-20 15:26:47 +0900

    Refactor : delete stock_sector and modify news tags

--- src/dags/dbt_dbt_top_sector_news_dag.py
+++ src/dags/dbt_dbt_top_sector_news_dag.py
@@ -27,6 +27,7 @@ dbt_top_sector_news_dag = DbtDag(
     schedule_interval="@daily",
     start_date=datetime(2023, 1, 1),
     catchup=False,
+    tags=['dbt','snowflake']
     dag_id="dbt_top_sector_news",
     render_config=RenderConfig(
         select=["+top_sector_company_news", "+top_sector", "+top_sector_news", "+stock_with_industry"],
