2603bb3a0483444572ad067fe25cf84ed303455d
JehyunP <parkjehyun0131@gmail.com>
2025-11-20 14:47:11 +0900

    Created dbt merging raw_data.stock_company and company details

--- src/dags/dbt_news_dag.py
+++ src/dags/dbt_news_dag.py
@@ -28,6 +28,6 @@ dbt_top_sector_news_dag = DbtDag(
     catchup=False,
     dag_id="dbt_top_sector_news",
     render_config=RenderConfig(
-        select=["+top_sector_company_news", "+top_sector", "+top_sector_news"],
+        select=["+top_sector_company_news", "+top_sector", "+top_sector_news", "+stock_with_industry"],
     ),
 )
